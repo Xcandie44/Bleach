dd09224fffb7c2419c054840e6ff23d3957dc386
pechn <pechinikov.ivan@yandex.by>
2018-05-20 13:09:08 +0300

    Bleach

--- app/src/main/java/spaseodysseyproject/bleach/MainActivity.java
+++ app/src/main/java/spaseodysseyproject/bleach/MainActivity.java
@@ -4,19 +4,24 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button b1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        b1 = (Button) findViewById(R.id.button);
+        b1.setVisibility(View.INVISIBLE);
+        b1.setEnabled(true);
     }
 
     public void onStartClick(View v){
         Intent intent = new Intent(this,Second.class);
         startActivity(intent);
+
 
     }
 }
